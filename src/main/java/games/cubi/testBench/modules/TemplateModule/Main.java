e800fb5184f6d734b57168b66c82498b68b318e5
Cubicake <70577278+Cubicake@users.noreply.github.com>
2025-08-26 17:31:20 +0930

    Remove static import

--- src/main/java/games/cubi/testBench/modules/TemplateModule/Main.java
+++ src/main/java/games/cubi/testBench/modules/TemplateModule/Main.java
@@ -1,9 +1,9 @@
 package games.cubi.testBench.modules.TemplateModule;
 
 import games.cubi.testBench.TestBench;
-import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 
-import static org.bukkit.Bukkit.getServer;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.bukkit.Bukkit;
 
 public class Main {
 
@@ -12,7 +12,7 @@ public class Main {
         // Plugin startup logic
         testBench.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event -> event.registrar().register("templatebench", new Commands()));
 
-        getServer().getPluginManager().registerEvents(new EventsHandler(), testBench);
+        Bukkit.getServer().getPluginManager().registerEvents(new EventsHandler(), testBench);
     }
 
     public static void onDisable() {
